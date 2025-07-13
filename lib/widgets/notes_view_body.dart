10889a271fdd514a9c8940a0bff9d6cdd68fe02d
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-07-13 19:15:12 +0300

    Replace NotesItem with NotesListView in notes_view_body.dart for improved functionality and layout.

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/widgets/custom_appbar_widget.dart';
-import 'package:notesapp/widgets/custom_note_item.dart';
+import 'package:notesapp/widgets/notes_listview.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -8,11 +8,10 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        const SizedBox(height: 50),
-        const CustomAppBar(),
-        NotesItem(),
-        //Row(children: [Text("Flutte tips"), CustomSearchIcon()]),
+      children: const [
+        SizedBox(height: 50),
+        CustomAppBar(),
+     Expanded(child: NotesListView()),
       ],
     );
   }
