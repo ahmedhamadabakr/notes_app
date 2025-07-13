132eb175b488a10be4be903a5da7546660df46ca
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-07-13 18:46:10 +0300

    Remove NotesItem widget from notes_view_body.dart and import custom_note_item.dart for improved modularity.

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(12),
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutte tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Text(
+              'Build your career with flutter tech',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 148, 187, 207),
+                fontSize: 18,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+              style: ButtonStyle(
+                iconColor: WidgetStateProperty.all(Colors.black),
+                iconSize: WidgetStatePropertyAll(30),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, top: 16),
+            child: Text(
+              DateTime.now().toString(),
+              style: TextStyle(color: const Color.fromARGB(106, 0, 0, 0)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/widgets/custom_appbar_widget.dart';
+import 'package:notesapp/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -17,51 +18,3 @@ class NotesViewBody extends StatelessWidget {
   }
 }
 
-class NotesItem extends StatelessWidget {
-  const NotesItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(12),
-      padding: const EdgeInsets.only(top: 24, bottom: 24,left: 12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              'Flutte tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Text(
-              'Build your career with flutter tech',
-              style: TextStyle(
-                color: const Color.fromARGB(255, 148, 187, 207),
-                fontSize: 18,
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete),
-              style: ButtonStyle(
-                iconColor: WidgetStateProperty.all(Colors.black),
-                iconSize: WidgetStatePropertyAll(35),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 20,top: 16),
-            child: Text(
-              DateTime.now().toString(),
-              style: TextStyle(color: const Color.fromARGB(106, 0, 0, 0)),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
