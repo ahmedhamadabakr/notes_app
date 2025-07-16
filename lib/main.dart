818377cb0f15d4f1f64aa784cbbda73e52173115
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-07-16 16:22:02 +0300

    Integrate MultiBlocProvider in main.dart to manage NotesCubit state, enhancing state management for the notes application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notesapp/constants.dart';
+import 'package:notesapp/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:notesapp/model/note_model.dart';
 import 'package:notesapp/views/notes_view.dart';
 
@@ -17,10 +19,13 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
-      home: NotesView(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => NotesCubit())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark),
+        home: NotesView(),
+      ),
     );
   }
 }
