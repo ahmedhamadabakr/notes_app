26bb9f4f40b09a9747f1f37f03c50af9d59dd25e
Ahmed Hamada Bakr <ahmedhamadabakr77@gmail.com>
2025-07-18 23:08:36 +0300

    Update NotesView to enable scrollable modal bottom sheet and refactor AddNoteButtonSheet for improved padding management. This enhances the user experience by ensuring the bottom sheet is fully accessible and visually consistent during note submission.

--- lib/widgets/add_note_button_sheet.dart
+++ lib/widgets/add_note_button_sheet.dart
@@ -2,35 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_notes_cubit.dart';
 import 'package:notesapp/widgets/add_note_form.dart';
-import 'dart:math';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(
-        left: 16,
-        right: 16,
-        bottom: max(0, MediaQuery.of(context).viewInsets.bottom),
-      ),
-      child: BlocConsumer<NotesCubit, AddNoteState>(
-        listener: (BuildContext context, state) {
-          if (state is AddNoteFailure) {
-            print("errrrorrrr  :  ${state.errMessage}");
-          }
-          if (state is AddNoteSuccess) {
-            Navigator.pop(context);
-          }
-        },
-        builder: (BuildContext context, state) {
-          return AbsorbPointer(
-            absorbing: state is AddNoteLoading ? true : false,
+    return BlocConsumer<NotesCubit, AddNoteState>(
+      listener: (BuildContext context, state) {
+        if (state is AddNoteFailure) {
+          print("errrrorrrr  :  ${state.errMessage}");
+        }
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (BuildContext context, state) {
+        return AbsorbPointer(
+          absorbing: state is AddNoteLoading ? true : false,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: 16,
+              right: 16,
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
             child: const SingleChildScrollView(child: AddNoteForm()),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
